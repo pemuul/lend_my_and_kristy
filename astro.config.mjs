f32122fab9910ed9be35944f5c3c573322c35fbd
pemuul <pemuul@inbox.ru>
2026-01-22 22:33:38 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
+// astro.config.mjs
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://pemuul.github.io",
+  base: isGitHubActions ? "/lend_my_and_kristy" : "/",
+  output: "static",
   integrations: [tailwind(), sitemap(), icon()],
-  output: "static"
 });
